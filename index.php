a65927b06845708b28c046b3b5969a0ba9b0d6e1
Kit <kit@getpantheon.com>
2017-05-01 16:23:03 -0700

    now with database storage

--- index.php
+++ index.php
@@ -5,32 +5,120 @@ define("UNDETECTED","Undetected");
 require_once 'sslLabsApi.php';
 //just the one argument, a URL
 $site= $argv[1];
-check_site($site);
-//check_ssl($site);
-//check_speed($site);
+$date = gmdate('Y-m-d H:i:s');
+$site_info = check_site($site);
+$site_info['date'] = $date;
+$ssl_scores = check_ssl($site);
+//$ssl_scores= NULL;
+$performance_scores = check_speed($site);
+write_checks($site_info,$ssl_scores,$performance_scores);
 
+function write_checks($site,$ssl_scores,$performance_scores){
+  //print_r($site);
+  //echo "\n\n";
+  $servername = "localhost";
+  $username = "root";
+  $password = "beeblebrox";
+  $dbname = "profile";
+// Create connection
+  $conn = mysqli_connect($servername, $username, $password,$dbname);
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+  $stmt = $conn->prepare("INSERT INTO site (date, url,provider, cms) VALUES (?,?,?,?)" );
+  $stmt->bind_param("ssss",$site['date'],$site['site'],$site['provider'],$site['cms']);
+
+  if ($stmt->execute() === TRUE) {
+    //  echo "New record created successfully";
+      $site_id = $stmt->insert_id;
+      if (count($site['features'])>0){
+        foreach($site['features'] as $feature){
+        //echo $feature,"\n";
+          $stmt = $conn->prepare("INSERT INTO features (site_id,feature) VALUES (?,?)" );
+          $stmt->bind_param("ss",$site_id,$feature);
+          $stmt->execute();
+        }
+      }
+      if (count($ssl_scores)>0){
+        //print_r($ssl_scores);
+        foreach($ssl_scores as $k=>$v){
+        //echo $feature,"\n";
+          $stmt = $conn->prepare("INSERT INTO security (site_id,endpoint, score) VALUES (?,?,?)" );
+          $stmt->bind_param("sss",$site_id,$k,$v);
+          $stmt->execute();
+        }
+      }
+      if (count($performance_scores)>0){
+        print_r($performance_scores);
+        $fields="";
+        $param="";
+        if ($performance_scores['runs'] > 1){
+          foreach($performance_scores as $k=>$v){
+            $fields .= $k . ",";
+            $param .= $v . ",";
+          }
+          $fields = "site_id," . rtrim($fields,",");
+          $param  = $site_id . ",".rtrim($param,",");
+          $sql = "INSERT INTO PERFORMANCE (" . $fields . ") VALUES (" . $param . ")";
+        }
+      }
+
+  } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $stmt->close();
+  $conn->close();
+  return 1;
+}
 function check_speed($site){
+  $results = array();
+  $runs = 2;
   if(`which sitespeed.io`){
     echo "Checking Site Performance\nBrowsers will spawn, Do not be alarmed.\n";
-    $rawresults = `sitespeed.io $site`;
+    $rawresults = `sitespeed.io $site -n $runs`;
     $temp = explode("\n",$rawresults);
     foreach($temp as $line){
       if(strpos($line,'backEndTime')){
-        $line = preg_replace("/\[.+\] INFO: /","",$line);
-        $results = "Performance Test Results: " . $line. "\n";
+        //echo $line;
+        $regex = "/([\d|\.]+)/";
+        preg_match_all($regex,$line,$matches,PREG_PATTERN_ORDER,24);
+        //print_r($matches);
+        if ($runs > 1){
+          $results['requests'] = $matches[0][0];
+          $results['size'] = $matches[0][1];
+          $results['backendtime'] = $matches[0][2];
+          $results['backendtime_deviation'] = $matches[0][3]/1000;
+          $results['firstpaint'] = $matches[0][4];
+          $results['firstpaint_deviation'] = $matches[0][5]/1000;
+          $results['domcontentloaded'] = $matches[0][6];
+          $results['domcontentloaded_deviation'] = $matches[0][7]/1000;
+          $results['pageload'] = $matches[0][8];
+          $results['pageload_deviation'] = $matches[0][9]/1000;
+          $results['rumspeedindex'] = $matches[0][10];
+          $results['rumspeedindex_deviation'] = $matches[0][11];
+          $results['runs'] = $matches[0][12];
+        } else {
+          $results['requests'] = $matches[0][0];
+          $results['size'] = $matches[0][1];
+          $results['backendtime'] = $matches[0][2];
+          $results['firstpaint'] = $matches[0][3];
+          $results['domcontentloaded'] = $matches[0][4];
+          $results['load'] = $matches[0][5];
+          $results['rumspeedindex'] = $matches[0][6];
+          $results['runs'] = 1;
+        }
         break;
-      } else {
-        $results = "Performance Test Failed\n";
       }
     }
-    echo $results;
   } else {
     echo "sitespeed.io not installed. Cannot run performance test\n";
   }
+  return $results;
 }
 
 function check_site($site){
   //init curl
+  $site_info= array();
   $ch = curl_init($site);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   //gotta catch those 301s
@@ -41,7 +129,7 @@ function check_site($site){
   $response = curl_exec($ch);
   $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
   $header_text = substr($response, 0, $header_size);
-  //echo "$header_text\n";
+  echo "Checking Site Features\n";
   //separate the body from the headers
   $body = substr($response, $header_size);
   $headers=parse_headers($header_text);
@@ -52,16 +140,26 @@ function check_site($site){
     $cms=check_body($body,"cms");
   }
   if ($cms=="WordPress" && $provider=="Undetected"){
-    echo "Checking for WP Engine in body\n";
+//    echo "Checking for WP Engine in body\n";
     $provider = check_body($body,"provider");
   }
   $headerfeatures = check_headers($headers,"feature");
   $bodyfeatures = check_body($body,"feature");
-  echo "\n$site is running $cms on $provider with $headerfeatures, $bodyfeatures \n\n";
+  $site_info['site'] = $site;
+  $site_info['cms'] = $cms[0];
+  $site_info['provider'] = $provider[0];
+  $site_info['features'] = $headerfeatures + $bodyfeatures;
+  if (count($site_info['features'])> 0){
+    $features=array_unique($site_info['features'],SORT_STRING);
+    $features = implode(", ",$features);
+  }
+  echo "\n$site is running $cms on $provider with $features \n\n";
+  return $site_info;
 }
 
 function check_ssl($host){
   if (substr($host,0,5) == 'https'){
+    $endpoint_scores = array();
   //Return API response as JSON string
     $api = new sslLabsApi();
     echo "Checking SSL Labs API Status";
@@ -90,6 +188,7 @@ function check_ssl($host){
       foreach ($hostinfo['endpoints'] as $endpoint){
         //print_r($endpoint);
         if($endpoint['statusMessage'] == "Ready"){
+          $endpoint_scores[$endpoint['ipAddress']] = $endpoint['grade'];
           echo "Endpoint: ", $endpoint['ipAddress']," - ", $endpoint['grade'], "\n";
         }
       }
@@ -99,13 +198,14 @@ function check_ssl($host){
   } else {
     echo "Cannot run SSL Scan on ", $host,"\n";
   }
+  return $endpoint_scores;
 }
 function check_body($body,$type){
   $features = array();
   $file = dirname(__FILE__). "/".$type ."body.json";
   $checks=json_decode(file_get_contents($file),true);
   foreach($checks as $k=>$v){
-    echo "Checking for $k in body \n";
+//    echo "Checking for $k in body \n";
     //print_r($v);
     foreach($v as $v2){
       //echo "checking for $v2 in body\n";
@@ -114,11 +214,11 @@ function check_body($body,$type){
       }
     }
   }
-  if (count($features)> 0){
+ if (count($features)> 0){
     $features=array_unique($features,SORT_STRING);
-    $features = implode(", ",$features);
+  //  $features = implode(", ",$features);
   } else {
-    $features = UNDETECTED;
+    $features = (array) UNDETECTED;
   }
   return $features;
 }
@@ -128,7 +228,7 @@ function check_headers($headers,$type){
   $file = dirname(__FILE__). "/".$type ."headers.json";
   $checks=json_decode(file_get_contents($file),true);
   foreach($checks as $k=>$v){
-    echo "Checking for $k in headers\n";
+//    echo "Checking for $k in headers\n";
     foreach($v as $k2=>$v2){
       //check only for header existence
       if ($v2 ==""){
@@ -138,7 +238,6 @@ function check_headers($headers,$type){
       } else {
         //echo "Key: $k2 , Value:$v2\n";
         //check the key and value of the header
-        //should probably use regex instead of stripos
         $respheader = $headers[strtolower($k2)];
         $v2 = "/".$v2."/";
         //echo "Header Key:$k2, Header Value: $respheader , Value: $v2\n";
@@ -151,9 +250,9 @@ function check_headers($headers,$type){
   }
   if (count($features)> 0){
     $features=array_unique($features,SORT_STRING);
-    $features = implode(", ",$features);
+  //  $features = implode(", ",$features);
   } else {
-    $features = UNDETECTED;
+    $features = (array) UNDETECTED;
   }
   return $features;
 }
